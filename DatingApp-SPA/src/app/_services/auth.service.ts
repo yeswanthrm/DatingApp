63a958ed915f94067913ca2278491d54219fb0f6
Yeswanth <yesuse@rediffmail.com>
2018-08-10 16:58:16 +0530

    EndOfstage6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '../../../node_modules/@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:58360/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
 constructor(private http: HttpClient) {}
 login(model: any) {
   return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -14,12 +18,18 @@ login(model: any) {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
       }
     })
   );
 }
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
