76ca2163ceb99acb17ee4cc222c9a7be5ebc9b76
Yeswanth <yesuse@rediffmail.com>
2018-08-19 17:51:33 +0530

    EndOfstg8_new

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -14,6 +14,7 @@ namespace DatingApp.API.Helpers
                 })
                 .ForMember(dest => dest.Age, opt => {
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
                 
             CreateMap<User, UserForDetailedDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => {
@@ -21,6 +22,7 @@ namespace DatingApp.API.Helpers
                 })
                 .ForMember(dest => dest.Age, opt => {
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
             CreateMap<Photo, PhotosForDetailedDto>();
         }
         
